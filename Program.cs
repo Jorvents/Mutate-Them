fefee9d068a14593e72b9be4894289146730f606
Jorvents <jorven.tsahkna@gmail.com>
2022-08-01 21:14:19 +0300

    First files

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Raylib_cs;
+
+namespace MutateThem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Raylib.InitWindow(800, 480, "Hello World");
+
+            while (!Raylib.WindowShouldClose())
+            {
+                Raylib.BeginDrawing();
+                Raylib.ClearBackground(Color.BLACK);
+
+                Raylib.DrawText("Hello, world!", 12, 12, 20, Color.WHITE);
+
+                Raylib.EndDrawing();
+            }
+
+            Raylib.CloseWindow();
+        }
+    }
+}
